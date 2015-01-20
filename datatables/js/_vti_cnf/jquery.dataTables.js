39b0ac2fee9baf748d87f95857ffb7864dd3597c
mdlawrenceusa <mdlawrence@me.com>
2015-01-20 10:25:39 -0500

    datatables second try

---
+++ datatables/js/_vti_cnf/jquery.dataTables.js
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TW|04 Oct 2014 15:56:21 -0000
+vti_author:SR|ECET\\Professor
+vti_modifiedby:SR|ECET\\Professor
+vti_nexttolasttimemodified:TW|04 Oct 2014 15:56:21 -0000
+vti_timecreated:TR|20 Jan 2015 15:23:55 -0000
+vti_cacheddtm:TX|20 Jan 2015 15:23:55 -0000
+vti_filesize:IR|443187
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|datatables/master.dwt
